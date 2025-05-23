2884d16a5dc3901b891d6d03f401ca82e090fe6e
KabeloMaps <mpnkab004@myuct.ac.za>
2025-05-23 18:39:44 +0200

    Completed general layout and style using tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // This scans all HTML files in the root directory
+  theme: {
+    extend: {
+      colors: {
+        customYellow: "#F4D04E",
+      },
+    },
+  },
+  plugins: [],
+};
